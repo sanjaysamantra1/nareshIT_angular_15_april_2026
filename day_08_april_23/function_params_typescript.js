3cf2bfedbc0d5b2878f2d2be6a6f7e82a10e6e8a
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-04-23 09:04:27 +0530

    day 8

---
+++ day_08_april_23/function_params_typescript.js
@@ -0,0 +1,5 @@
+function add(a, b, c) {
+    console.log(a + b + c);
+}
+add(10, 20); // NaN
+add(10, 20, 30, 40); // 60
---
+++ day_08_april_23/function_params_typescript.ts
@@ -0,0 +1,5 @@
+function add(a: number, b: number, c: number) {
+  console.log(a + b + c);
+}
+add(10, 20); // NaN
+add(10, 20, 30, 40); // 60
